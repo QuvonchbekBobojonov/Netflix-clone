a33446a830471f12154eaf0c13ac4459b8d3fb7d
Quvonchbek Bobojonov <moorfoinfo@gmail.com>
2024-05-03 10:37:20 +0500

    created page

--- components/shared/login.tsx
+++ components/shared/login.tsx
@@ -1,7 +1,26 @@
+import Image from 'next/image';
+import { FaGithub } from "react-icons/fa";
+import {Button} from "@/components/ui/button";
+
 export const Login = () => {
     return (
-        <div>
-            Login component
+        <div className={'w-full h-screen'}>
+            <div className={'absolute inset-0'}>
+                <Image
+                    src={"https://repository-images.githubusercontent.com/299409710/b42f7780-0fe1-11eb-8460-e459acd20fb4"}
+                    alt={"bg"}
+                    fill
+                />
+            </div>
+            <div className={'relative z-10 w-[500px] bg-black/60 rounded-md h-[50vh] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 px-8 py-4'}>
+                <div className={'flex h-full justify-center items-center flex-col'}>
+                    <Button
+                        className={'mt-4 flex items-center gap-2 w-full h-[56px] bg-red-600 text-white hover:bg-red-500'}>
+                        <FaGithub className={'h-7 w-7'}/>
+                        Sign in with Github
+                    </Button>
+                </div>
+            </div>
         </div>
     );
 };
